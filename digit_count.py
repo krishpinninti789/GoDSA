d0fae2cec84a71b79ef58c24d3893ae11fd36880
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2025-08-05 15:46:56 +0530

    optimize the digit_count using log

--- digit_count.py
+++ digit_count.py
@@ -1,11 +1,18 @@
+from math import *
+
 def count_digits(num):
-    count=0
-    if num<0:
-        num = -num
-    while num>0:
-        num = num//10
-        count+=1
-    return count
+    # count=0
+    # if num<0:
+    #     num = -num
+    # while num>0:
+    #     num = num//10
+    #     count+=1
+    # return count
+    
+    
+    #log approach
+    
+    return int(log10(num)+1)
 
 n = int(input("Enter the number: "))
 print("Number of digits are: ",count_digits(n))
