60565f66996bff6963b6ec3b754f2ae0489e8152
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2025-09-05 22:50:41 +0530

    search in the rotated sorted array

---
+++ search_rotated.py
@@ -0,0 +1,23 @@
+def search(nums,target):
+    
+    low = 0
+    high = len(nums)-1
+    while low<=high:
+        mid = (low+high)//2
+        if nums[mid]==target:
+            return mid
+        if nums[mid]<=nums[high]:
+            if nums[mid]<=target<=nums[high]:
+                low=mid+1
+            else:
+                high = mid-1
+        else:
+            if nums[low]<=target<=nums[mid]:
+                high = mid-1
+            else:
+                low = mid+1
+    return -1
+    
+nums = list(map(int,input("Enter the numbers: ").split(" ")))
+target = int(input("Enter the target element: "))
+print(search(nums,target))
